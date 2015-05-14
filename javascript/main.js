6b6c41f07b950801d70a68e423c08bdbe203eed9
alu0100315462 <alu0100315462@ull.edu.es>
2015-05-14 09:34:12 +0000

    boton para descargar la salida

--- javascript/main.js
+++ javascript/main.js
@@ -8,3 +8,34 @@ $(document).ready(function () {
     }
   });
 });
+
+
+$(document).ready(function () {
+  $('#BOT_DESCARGAR').click(function saveTextAsFile()
+{
+    var textToWrite = $('#TA_OUTPUT').val();
+    var textFileAsBlob = new Blob([textToWrite], {type:'text/plain'});
+    var fileNameToSaveAs = "formulario.html"
+
+    var downloadLink = document.createElement("a");
+    downloadLink.download = fileNameToSaveAs;
+    downloadLink.innerHTML = "Download File";
+    if (window.webkitURL != null)
+    {
+        // Chrome allows the link to be clicked
+        // without actually adding it to the DOM.
+        downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+    }
+    else
+    {
+        // Firefox requires the link to be added to the DOM
+        // before it can be clicked.
+        downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+        downloadLink.onclick = destroyClickedElement;
+        downloadLink.style.display = "none";
+        document.body.appendChild(downloadLink);
+    }
+
+    downloadLink.click();
+});
+});
