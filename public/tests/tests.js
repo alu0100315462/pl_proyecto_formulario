c98998ced1d29046ea2061bf6f04b213d93dcbf4
gcpmendez <gcpmendez@gmail.com>
2015-05-15 00:27:28 +0000

    Añadido segundo test con éxito

--- public/tests/tests.js
+++ public/tests/tests.js
@@ -13,4 +13,11 @@ suite('Generador de formularios', function() {
       result = parser.parse('!title "Titulo de prueba"\n');
       return assert.deepEqual(result, "<html>\n<head><meta charset=\'utf-8\'></head>\n<body>\n&lt;p&gt;&lt;h1&gt;Titulo de prueba&lt;/h1&gt;&lt;/p&gt;\n&lt;/body&gt;\n&lt;/html&gt;\n\n");
     });
+    
+    test("Prueba de !description", function() {
+      var result;
+      result = parser.parse('!title "Titulo de prueba"\n !description "Texto de explicacion de prueba."\n');
+      return assert.deepEqual(result, "<html>\n<head><meta charset=\'utf-8\'></head>\n<body>\n&lt;p&gt;&lt;h1&gt;Titulo de prueba&lt;/h1&gt;&lt;/p&gt;\n&lt;p&gt;Texto de explicacion de prueba.&lt;/p&gt;\n&lt;/body&gt;\n&lt;/html&gt;\n\n");
+    });
+    
 });
