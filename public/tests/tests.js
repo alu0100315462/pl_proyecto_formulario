348c4818f1142ed8559a66444ab2149aa157dfc6
gcpmendez <gcpmendez@gmail.com>
2015-05-15 01:10:07 +0000

    Modificado Package.json

--- public/tests/tests.js
+++ public/tests/tests.js
@@ -2,7 +2,7 @@ var assert, chai;
   chai = require('chai');
   assert = chai.assert;
   
-suite('Generador de formularios', function() {
+describe('Generador de formularios', function() {
 
   setup(function(){
     if (typeof __html__ !== 'undefined') {
@@ -10,13 +10,13 @@ suite('Generador de formularios', function() {
     }
   });
   
-    test("Prueba de !title", function() {
+    it("Prueba de !title", function() {
       var result;
       result = parser.parse('!title "Titulo de prueba"\n');
       return assert.deepEqual(result, "<html>\n<head><meta charset=\'utf-8\'></head>\n<body>\n&lt;p&gt;&lt;h1&gt;Titulo de prueba&lt;/h1&gt;&lt;/p&gt;\n&lt;/body&gt;\n&lt;/html&gt;\n\n");
     });
     
-    test("Prueba de !description", function() {
+    it("Prueba de !description", function() {
       var result;
       result = parser.parse('!title "Titulo de prueba"\n !description "Texto de explicacion de prueba."\n');
       return assert.deepEqual(result, "<html>\n<head><meta charset=\'utf-8\'></head>\n<body>\n&lt;p&gt;&lt;h1&gt;Titulo de prueba&lt;/h1&gt;&lt;/p&gt;\n&lt;p&gt;Texto de explicacion de prueba.&lt;/p&gt;\n&lt;/body&gt;\n&lt;/html&gt;\n\n");
