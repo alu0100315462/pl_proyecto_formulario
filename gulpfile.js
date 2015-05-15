a10e2b3bd7012948167a95bd02ded4ff25b4277f
gcpmendez <gcpmendez@gmail.com>
2015-05-15 01:31:40 +0000

    Arreglando travis

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp    = require('gulp'),
+gutil   = require('gulp-util'),
+uglify  = require('gulp-uglify'),
+concat  = require('gulp-concat');
+var del     = require('del');
+var minifyHTML = require('gulp-minify-html');
+var minifyCSS  = require('gulp-minify-css');
+var run = require('gulp-run');
+var karma = require('karma').server;
+
+gulp.task('minify', function () {
+  gulp.src('./app.coffee')
+  .pipe(uglify())
+  .pipe(gulp.dest('minified'));
+
+  gulp.src('./public/javascript/*.js')
+  .pipe(uglify())
+  .pipe(gulp.dest('minified'));
+
+  gulp.src('./views/*.jade')
+  .pipe(minifyHTML())
+  .pipe(gulp.dest('./minified/'))
+
+  gulp.src('./public/stylesheets/*.css')
+  .pipe(minifyCSS({keepBreaks:true}))
+  .pipe(gulp.dest('./minified/'))
+});
+
+gulp.task('clean', function(cb) {
+  del(['minified/*'], cb);
+});
+
+gulp.task('tests', function() {
+  run('mocha --compilers coffee:coffee-script/register -R spec public/tests/tests.coffee').exec()
+});
+
+gulp.task('default', function(done) {
+  run('mocha --compilers coffee:coffee-script/register -R spec public/tests/tests.js').exec()
+});
